2fac695b5473aec89244fbb4c974e591b2880cab
jeevabharathi276 <jeevabharathi87@gmail.com>
2024-08-06 17:59:06 +0530

    project added

--- Angularmicroproject/src/app/app.module.ts
+++ Angularmicroproject/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser'
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieService } from './movie.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http'; // Correct import
 
 @NgModule({
   declarations: [
